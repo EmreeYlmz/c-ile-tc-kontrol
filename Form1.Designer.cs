e5fe071fca247238ed10a10640f85d791044692a
EMRE YILMAZ <130606706+EmreeYlmz@users.noreply.github.com>
2024-09-17 17:16:18 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace tckontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Blue;
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(82, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kontrol Et";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TC kontrol ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 75;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(242, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "KONRTOL ETMEK İSTEDİĞİNİZ TC Yİ GİRİNİZ:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Maroon;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TC Kontrol";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tckontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string tc = textBox1.Text;
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+            if (e.KeyChar == (char)Keys.Back)
+            {
+                e.Handled = false;
+
+            }
+
+            else if (textBox1.Text.Length == 0)
+            {
+                if (e.KeyChar == '0')
+                {
+                    e.Handled = true;
+                    errorProvider1.SetError(textBox1, "T.C 0 ile başlamaz");
+                }
+                else
+                    errorProvider1.Clear();
+            }
+            else if (textBox1.Text.Length == 9)
+            {
+
+                int[] dizi = new int[textBox1.Text.Length];
+                for (int i = 0; i < textBox1.Text.Length; i++)
+                {
+                    dizi[i] = (int)char.GetNumericValue(textBox1.Text[i]);
+                }
+
+                int ilk = 0;
+                for (int i = 0; i <= 8; i += 2)
+                {
+                    ilk += dizi[i];
+                }
+                ilk *= 7;
+
+                int son = 0;
+                for (int i = 1; i <= 7; i += 2)
+                {
+                    son += dizi[i];
+                }
+
+                int hane_10 = (ilk - son) % 10;
+                if (e.KeyChar.ToString() != hane_10.ToString())
+                {
+                    e.Handled = true;
+                    errorProvider1.SetError(textBox1, "10.hane yanlış");
+                }
+                else
+                {
+                    e.Handled = false;
+                    errorProvider1.Clear();
+                }
+
+            }
+            else if (textBox1.Text.Length == 10)
+            {
+
+                int[] dizi = new int[textBox1.Text.Length];
+                for (int i = 0; i < textBox1.Text.Length; i++)
+                {
+                    dizi[i] = (int)char.GetNumericValue(textBox1.Text[i]);
+                }
+                int toplam = 0;
+                for (int i = 0; i <= 9; i++)
+                {
+                    toplam += dizi[i];
+                }
+
+                int hane_11 = toplam % 10;
+                if (e.KeyChar.ToString() != hane_11.ToString())
+                {
+                    e.Handled = true;
+                    errorProvider1.SetError(textBox1, "11.hane yanlış");
+                }
+                else
+                    errorProvider1.Clear();
+            }
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += 5;
+            if (label1.Left > 300)
+                label1.Left = -70;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (errorProvider1.GetError(textBox1) == string.Empty && textBox1.Text.Length == 11)
+            {
+                MessageBox.Show("T.C. Kimlik Numarası Doğru", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("T.C. Kimlik Numarasında Hata Var", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+
+        }
+    }
+
+
+
+    }
+
